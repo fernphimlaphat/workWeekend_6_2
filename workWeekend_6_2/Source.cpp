2086051825d843fda9b5dee9a6db60e04347e8e2
Phimlaphat Surayaem <fernphimlaphat_44@hotmail.com>
2020-09-17 20:15:20 +0700

    Add project files.

---
+++ workWeekend_6_2/Source.cpp
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<math.h>
+
+int Multi(int num)
+{
+	int  sum = 1, num1[100];
+
+	for (int i = 0; i < num; i++)
+	{
+		num1[i] = i + 1;
+		//printf("%d", num1[i]);
+		sum = sum * num1[i];
+		//printf("%d ", sum);
+	}
+	return sum;
+}
+
+int main()
+{
+	int num, i, sum = 0, num1;
+	printf("Enter number : ");
+	scanf_s("%d", &num);
+	num1 = num;
+	if (num >= 0) {
+		do
+		{
+			Multi(num);
+			//printf("%d ", Multi(num));
+			sum += Multi(num);
+			num--;
+
+		} while (num > 0);
+
+		printf("1! + ");
+
+		for (int i = 2; i < num1; i++)
+		{
+			printf("%d! + ", i);
+		}
+		printf("%d!", num1);
+		printf(" = %d", sum);
+	}
+	else
+	{
+		do {
+			printf("Try again : ");
+			scanf_s("%d", &num);
+			num1 = num;
+		} while (num < 0);
+
+
+			if (num >= 0) {
+				do
+				{
+					Multi(num);
+					//printf("%d ", Multi(num));
+					sum += Multi(num);
+					num--;
+
+				} while (num > 0);
+
+				printf("1! + ");
+
+				for (int i = 2; i < num1; i++)
+				{
+					printf("%d! + ", i);
+				}
+				printf("%d!", num1);
+				printf(" = %d", sum);
+			}
+		
+		
+
+	}
+
+}
